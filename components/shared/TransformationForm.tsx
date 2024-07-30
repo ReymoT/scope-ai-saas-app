0936566e7977eae5a4d815ce0254a5fb3c098ad3
ReymoT <tamme.reimo@gmail.com>
2024-07-30 22:26:57 +0300

    Added payment processor, resolved all issues with generating images, finalized UI

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import React, { useState, useTransition } from 'react';
+import React, { useEffect, useState, useTransition } from 'react';
 
 import {
     Select,
@@ -31,6 +31,10 @@ import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updateCredits } from "@/lib/actions/user.actions";
 import MediaUploader from "./MediaUploader";
 import TransformedImage from "./TransformedImage";
+import { getCldImageUrl } from "next-cloudinary";
+import { addImage, updateImage } from "@/lib/actions/image.actions";
+import { useRouter } from "next/navigation";
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal";
 
 export const formSchema = z.object({
     title: z.string(),
@@ -49,6 +53,7 @@ userId, type, creditBalance, config = null }: TransformationFormProps) => {
     const [isTransforming, setIsTransforming] = useState(false);
     const [transformationConfig, setTransformationConfig] = useState(config);
     const [isPending, startTransition] = useTransition();
+    const router = useRouter();
 
     const initialValues = data && action === 'Update' ? {
         title: data?.title,
@@ -57,17 +62,74 @@ userId, type, creditBalance, config = null }: TransformationFormProps) => {
         prompt: data?.prompt,
         publicId: data?.publicId,
     } : defaultValues;
-     // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialValues,
   })
  
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsSubmitting(true);
+
+    if (data || image) {
+        const transformationUrl = getCldImageUrl({
+            width: image?.width,
+            height: image?.height,
+            src: image?.publicId,
+            ...transformationConfig
+        })
+
+        const imageData = {
+            title: values.title,
+            publicId: image?.publicId,
+            transformationType: type,
+            width: image?.width,
+            height: image?.height,
+            config: transformationConfig,
+            secureURL: image?.secureURL,
+            transformationURL: transformationUrl,
+            aspectRatio: values.aspectRatio,
+            prompt: values.prompt,
+            color: values.color,
+        }
+
+        if (action === 'Add') {
+            try {
+                const newImage = await addImage({
+                    image: imageData,
+                    userId,
+                    path: '/'
+                });
+
+                if (newImage) {
+                    form.reset();
+                    setImage(data);
+                    router.push(`/transformations/${newImage._id}`);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        if (action === 'Update') {
+            try {
+                const updatedImage = await updateImage({
+                    image: {
+                        ...imageData,
+                        _id: data._id
+                    },
+                    userId,
+                    path: `/transformations/${data._id}`
+                });
+
+                if (updatedImage) {
+                    router.push(`/transformations/${updatedImage._id}`);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+    setIsSubmitting(false);
   }
 
   const onSelectFieldHandler = (value: string, onChangeField: (value: string) => void) => {
@@ -96,8 +158,9 @@ userId, type, creditBalance, config = null }: TransformationFormProps) => {
                     value
                 }
             }))
-            return onChangeField(value);
-        }, 1000);
+        }, 1000)();
+
+        return onChangeField(value);
     }
 
   const onTransformHandler = async () => {
@@ -110,13 +173,20 @@ userId, type, creditBalance, config = null }: TransformationFormProps) => {
     setNewTransformation(null);
 
     startTransition(async () => {
-        await updateCredits(userId, -1);
+        await updateCredits(userId, creditFee);
     })
   }
+
+  useEffect(() => {
+    if (image && (type === 'restore' || type === 'removeBackground')) {
+        setNewTransformation(transformationType.config);
+    }
+}, [image, transformationType.config, type])
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
         <CustomField
             control={form.control}
             name="title"
@@ -136,6 +206,7 @@ userId, type, creditBalance, config = null }: TransformationFormProps) => {
                         onValueChange={(value) => {
                             onSelectFieldHandler(value, field.onChange)
                         }}
+                        value={field.value}
                     >
                         <SelectTrigger className="select-field">
                             <SelectValue placeholder="Select size" />
--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,5 +1,5 @@
-import { dataUrl, debounce, getImageSize } from '@/lib/utils'
-import { CldImage } from 'next-cloudinary'
+import { dataUrl, debounce, download, getImageSize } from '@/lib/utils'
+import { CldImage, getCldImageUrl } from 'next-cloudinary'
 import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props'
 import Image from 'next/image'
 import React from 'react'
@@ -13,8 +13,15 @@ const TransformedImage = ({
     transformationConfig,
     hasDownload = false }: TransformedImageProps) => {
     
-        const downloadHandler = () => {
+        const downloadHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+            e.preventDefault();
 
+            download(getCldImageUrl({
+                width: image?.width,
+                height: image?.height,
+                src: image?.publicId,
+                ...transformationConfig
+            }), title);
         }
     
         return (
@@ -56,7 +63,7 @@ const TransformedImage = ({
                                     onError={() => {
                                         debounce(() => {
                                             setIsTransforming && setIsTransforming(false);
-                                        }, 8000)
+                                        }, 8000)()
                                     }}
                                     {...transformationConfig}
                                 />
@@ -67,8 +74,9 @@ const TransformedImage = ({
                                             src="/assets/icons/spinner.svg"
                                             width={50}
                                             height={50}
-                                            alt="Transforming"
+                                            alt="spinner"
                                         />
+                                        <p className='text-white/80'>Please wait...</p>
                                     </div>
                                 )}
                     </div>
